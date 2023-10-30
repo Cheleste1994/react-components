48790510bfef975409f922caca2934056faf3de3
Cheleste1994 <wolverines@tut.by>
2023-10-30 21:24:30 +0300

    feat: migrate in function component

--- src/components/Cards/SpeciesCard.tsx
+++ src/components/Cards/SpeciesCard.tsx
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ApiResponse, AppProps, Species } from '../../types/interface';
 import LogoLoad from '../LogoLoad/LogoLoad';
 import styles from '../Main/main.module.scss';
 
-export default class SpeciesCard extends Component<AppProps> {
-  render() {
-    return (
-      <div className={this.props.isLoading ? styles.loading : ''}>
-        {(this.props.dataResponse as ApiResponse<Species>)?.results?.map(
-          (el, index) => (
-            <div key={`${new Date().getTime()}${index}${Math.random()}`}>
-              <h3>{el.name}</h3>
-              <span>Classification: {el.classification}</span>
-              <span>Designation: {el.designation}</span>
-              <span className={styles.films}>
-                Film URL Resources that this starship has appeared in:
-                <div>
-                  {el.films?.map((film, index) => (
-                    <div key={`film-${index}`}>
-                      Film {index + 1}: <a href={film}>Link</a>
-                    </div>
-                  ))}
+export default function SpeciesCard({ isLoading, dataResponse }: AppProps) {
+  return (
+    <div className={isLoading ? styles.loading : ''}>
+      {(dataResponse as ApiResponse<Species>)?.results?.map((el, index) => (
+        <div key={`${new Date().getTime()}${index}${Math.random()}`}>
+          <h3>{el.name}</h3>
+          <span>Classification: {el.classification}</span>
+          <span>Designation: {el.designation}</span>
+          <span className={styles.films}>
+            Film URL Resources that this starship has appeared in:
+            <div>
+              {el.films?.map((film, index) => (
+                <div key={`film-${index}`}>
+                  Film {index + 1}: <a href={film}>Link</a>
                 </div>
-              </span>
-              <span>{el.created}</span>
+              ))}
             </div>
-          )
-        ) || <LogoLoad />}
-      </div>
-    );
-  }
+          </span>
+          <span>{el.created}</span>
+        </div>
+      )) || <LogoLoad />}
+    </div>
+  );
 }
