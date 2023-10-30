4e742e0236225ffdfe608f45d44470771f28ebbb
Cheleste1994 <wolverines@tut.by>
2023-10-30 21:21:53 +0300

    feat: migrate in function component

--- src/components/Cards/FilmCard.tsx
+++ src/components/Cards/FilmCard.tsx
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ApiResponse, AppProps, Film } from '../../types/interface';
 import LogoLoad from '../LogoLoad/LogoLoad';
 import styles from '../Main/main.module.scss';
 
-export default class FilmCard extends Component<AppProps> {
-  render() {
-    return (
-      <div className={this.props.isLoading ? styles.loading : ''}>
-        {(this.props.dataResponse as ApiResponse<Film>)?.results?.map(
-          (el, index) => (
-            <div key={`${new Date().getTime()}${index}${Math.random()}`}>
-              <h3>{el.title}</h3>
-              <span>Director: {el.director}</span>
-              <span>Producer: {el.producer}</span>
-              <span className={styles.films}>
-                Planet resource URLs that are in this film:
-                <div>
-                  {el.planets?.map((planet, index) => (
-                    <div key={`planet-${index}`}>
-                      Planet {index + 1}: <a href={planet}>Link</a>
-                    </div>
-                  ))}
+export default function FilmCard({ isLoading, dataResponse }: AppProps) {
+  return (
+    <div className={isLoading ? styles.loading : ''}>
+      {(dataResponse as ApiResponse<Film>)?.results?.map((el, index) => (
+        <div key={`${new Date().getTime()}${index}${Math.random()}`}>
+          <h3>{el.title}</h3>
+          <span>Director: {el.director}</span>
+          <span>Producer: {el.producer}</span>
+          <span className={styles.films}>
+            Planet resource URLs that are in this film:
+            <div>
+              {el.planets?.map((planet, index) => (
+                <div key={`planet-${index}`}>
+                  Planet {index + 1}: <a href={planet}>Link</a>
                 </div>
-              </span>
-              <span>{el.created}</span>
+              ))}
             </div>
-          )
-        ) || <LogoLoad />}
-      </div>
-    );
-  }
+          </span>
+          <span>{el.created}</span>
+        </div>
+      )) || <LogoLoad />}
+    </div>
+  );
 }
