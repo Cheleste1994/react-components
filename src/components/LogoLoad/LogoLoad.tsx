ab5db4492867dee919f70dab0d76d0b6630ee2a6
Cheleste1994 <wolverines@tut.by>
2023-10-30 21:26:41 +0300

    feat: migrate in function component

--- src/components/LogoLoad/LogoLoad.tsx
+++ src/components/LogoLoad/LogoLoad.tsx
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import icons from '../../assets/react.svg';
 import styles from './logoLoad.module.scss';
 
-export default class LogoLoad extends Component {
-  render() {
-    return <img src={icons} alt="loading" className={styles.logo} />;
-  }
+export default function LogoLoad() {
+  return <img src={icons} alt="loading" className={styles.logo} />;
 }
