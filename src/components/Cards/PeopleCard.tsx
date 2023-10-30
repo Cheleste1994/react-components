dafa7c81968fd702b7d49f8137eeee8f9fabfcf9
Cheleste1994 <wolverines@tut.by>
2023-10-30 21:22:56 +0300

    feat: migrate in function component

--- src/components/Cards/PeopleCard.tsx
+++ src/components/Cards/PeopleCard.tsx
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ApiResponse, AppProps, People } from '../../types/interface';
 import LogoLoad from '../LogoLoad/LogoLoad';
 import styles from '../Main/main.module.scss';
 
-export default class PeopleCard extends Component<AppProps> {
-  render() {
-    return (
-      <div>
-        {(this.props.dataResponse as ApiResponse<People>)?.results?.map(
-          (el, index) => (
-            <div
-              key={`${new Date().getTime()}${index}${Math.random()}`}
-              className={this.props.isLoading ? styles.loading : ''}
-            >
-              <h3>{el.name}</h3>
-              <span className={styles.films}>
-                Film resource URLs that this person has been in:
-                <div>
-                  {el.films?.map((film, index) => (
-                    <div key={`film-${index}`}>
-                      Film {index + 1}: <a href={film}>Link</a>
-                    </div>
-                  ))}
+export default function PeopleCard({ isLoading, dataResponse }: AppProps) {
+  return (
+    <div>
+      {(dataResponse as ApiResponse<People>)?.results?.map((el, index) => (
+        <div
+          key={`${new Date().getTime()}${index}${Math.random()}`}
+          className={isLoading ? styles.loading : ''}
+        >
+          <h3>{el.name}</h3>
+          <span className={styles.films}>
+            Film resource URLs that this person has been in:
+            <div>
+              {el.films?.map((film, index) => (
+                <div key={`film-${index}`}>
+                  Film {index + 1}: <a href={film}>Link</a>
                 </div>
-              </span>
-              <span>{el.created}</span>
+              ))}
             </div>
-          )
-        ) || <LogoLoad />}
-      </div>
-    );
-  }
+          </span>
+          <span>{el.created}</span>
+        </div>
+      )) || <LogoLoad />}
+    </div>
+  );
 }
