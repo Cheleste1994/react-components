b57b7059359152f32f59ffd2639ac59aaa4301c1
Cheleste1994 <wolverines@tut.by>
2023-10-30 22:14:46 +0300

    feat: migrate in function component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,86 +1,77 @@
-import React, { Component, ReactNode } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppProps, AppState } from '../../types/interface';
 import styles from './Header.module.scss';
 import svg from '../../assets/search.svg';
 
-export default class Header extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      dataRoot: null,
-      isLoading: true,
-      inputValue: localStorage.getItem('inputValue') || '',
-      selectValue: '',
-    };
+// extends Component<AppProps, AppState>
+
+export default function Header({
+  dataRoot,
+  selectValue,
+  isLoading,
+  updateInputValue,
+  updateSelectValue,
+}: AppProps) {
+  const [state, setState] = useState<AppState>({
+    dataRoot: null,
+    isLoading: true,
+    selectValue: '',
+  });
+
+  const [inputValue, setInputValue] = useState(
+    localStorage.getItem('inputValue') || ''
+  );
+
+  useEffect(() => {
+    setState({
+      dataRoot,
+      isLoading: false,
+    });
+  }, [dataRoot]);
+
+  let valueOption: string[] | [] = [];
+
+  if (state.dataRoot) {
+    valueOption = Object.keys(state.dataRoot);
   }
 
-  componentDidUpdate(prevProps: AppProps) {
-    if (this.props.dataRoot !== prevProps.dataRoot) {
-      this.setState({
-        dataRoot: this.props.dataRoot,
-        isLoading: false,
-      });
-    }
-  }
-
-  render(): ReactNode {
-    const { dataRoot, inputValue } = this.state;
-    let valueOption: string[] | [] = [];
-
-    if (dataRoot) {
-      valueOption = Object.keys(dataRoot);
-    }
-
-    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-      this.setState({ inputValue: event.target.value });
-    };
-
-    const handleSearchClick = (
-      event?: React.KeyboardEvent<HTMLInputElement>
-    ) => {
-      if (!event || event?.code === 'Enter') {
-        if (this.props.selectValue) {
-          this.props.updateInputValue?.(this.state.inputValue?.trim() || '');
-        }
+  const handleSearchClick = (event?: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!event || event?.code === 'Enter') {
+      if (selectValue) {
+        updateInputValue?.(inputValue?.trim() || '');
       }
-    };
+    }
+  };
 
-    const handleSelectChange = (
-      event: React.ChangeEvent<HTMLSelectElement>
-    ) => {
-      this.setState({ inputValue: '', selectValue: event.target.value });
-      this.props.updateSelectValue?.(event.target.value);
-    };
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setState({ ...state, selectValue: event.target.value });
+    updateSelectValue?.(event.target.value);
+  };
 
-    return (
-      <header className={styles.header}>
-        <div>React. Components</div>
-        <div>
-          <img src={svg} alt="search" onClick={() => handleSearchClick()} />
-          <select value={this.props.selectValue} onChange={handleSelectChange}>
-            <option disabled={!!this.props.selectValue}>
-              {this.state.isLoading ? 'Loading...' : 'Section'}
-            </option>
-            {valueOption.map((value, index) => (
-              <option key={value + index}>{value}</option>
-            ))}
-          </select>
-          <input
-            list="starWars"
-            placeholder={
-              this.state.isLoading
-                ? 'Loading...'
-                : this.props.selectValue
-                ? ''
-                : 'Select section'
-            }
-            value={inputValue}
-            onChange={handleInput}
-            disabled={this.props.isLoading}
-            onKeyDown={handleSearchClick}
-          />
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className={styles.header}>
+      <div>React. Components</div>
+      <div>
+        <img src={svg} alt="search" onClick={() => handleSearchClick()} />
+        <select value={selectValue} onChange={handleSelectChange}>
+          <option disabled={!!selectValue}>
+            {state.isLoading ? 'Loading...' : 'Section'}
+          </option>
+          {valueOption.map((value, index) => (
+            <option key={value + index}>{value}</option>
+          ))}
+        </select>
+        <input
+          list="starWars"
+          placeholder={
+            state.isLoading ? 'Loading...' : selectValue ? '' : 'Select section'
+          }
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          disabled={isLoading}
+          onKeyDown={handleSearchClick}
+        />
+      </div>
+    </header>
+  );
 }
