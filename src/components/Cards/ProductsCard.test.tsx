d6e8079182febc77a861e030b0159e8a2f4995e4
Cheleste1994 <wolverines@tut.by>
2023-11-10 19:23:18 +0300

    feat: add test in productsCard

--- src/components/Cards/ProductsCard.test.tsx
+++ src/components/Cards/ProductsCard.test.tsx
@@ -1,17 +1,44 @@
-import { render } from '@testing-library/react';
-
-import '@testing-library/jest-dom';
-
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter } from 'react-router-dom';
+import { ApiResponseState } from '../../types/interface';
+import { Context } from '../Context/Context';
 import ProductsCard from './ProductsCard';
 
-describe('<ProductsCard />', () => {
-  it('renders without errors', () => {
-    render(
-      <MemoryRouter>
+test('renders the specified number of cards', () => {
+  const dataSearch: ApiResponseState = {
+    isLoading: false,
+    dataResponse: {
+      total: 100,
+      skip: 0,
+      limit: 10,
+      products: [
+        {
+          id: 1,
+          title: 'Product 1',
+          brand: 'Brand 1',
+          category: 'Category 1',
+        },
+        {
+          id: 2,
+          title: 'Product 2',
+          brand: 'Brand 2',
+          category: 'Category 2',
+        },
+      ],
+    },
+  };
+
+  render(
+    <MemoryRouter>
+      <Context.Provider value={{ dataSearch }}>
         <ProductsCard />
-      </MemoryRouter>
-    );
+      </Context.Provider>
+    </MemoryRouter>
+  );
+
+  dataSearch.dataResponse?.products.forEach((product) => {
+    const titleElement = screen.getByText(product.title);
+    expect(titleElement).toBeInTheDocument();
   });
 });
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -26,15 +26,15 @@ export interface IdResponseState {
 export interface Product {
   id: number;
   title: string;
-  description: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: string[];
+  description?: string;
+  price?: number;
+  discountPercentage?: number;
+  rating?: number;
+  stock?: number;
+  brand?: string;
+  category?: string;
+  thumbnail?: string;
+  images?: string[];
 }
 
 export interface ProductList {
