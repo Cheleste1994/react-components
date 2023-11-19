8614c1d4287904291fad2c53b46862eeeb78aa90
Cheleste1994 <wolverines@tut.by>
2023-11-19 14:09:57 +0300

    feat: add tests rtk query

--- src/redux/tests/api.test.tsx
+++ src/redux/tests/api.test.tsx
@@ -2,7 +2,10 @@ import { renderHook } from '@testing-library/react';
 import React from 'react';
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { useGetProductsQuery } from '../api/productsApi';
+import {
+  useGetProductDetailQuery,
+  useGetProductsQuery,
+} from '../api/productsApi';
 import { store } from '../store';
 
 function Wrapper(props: { children: ReactNode }) {
@@ -21,9 +24,33 @@ beforeAll(() => {
 });
 
 describe('Api', () => {
-  it('useGetProductsQuery', () => {
-    renderHook(() => useGetProductsQuery('search?q=Test'), {
+  it('should "useGetProductsQuery" return result correct', () => {
+    const { result } = renderHook(() => useGetProductsQuery('search?q=Test'), {
       wrapper: Wrapper,
+    });
+
+    expect(result.current).toMatchObject({
+      status: 'pending',
+      endpointName: 'getProducts',
+      isLoading: true,
+      isSuccess: false,
+      isError: false,
+      isFetching: true,
+    });
+  });
+
+  it('should "useGetProductsQuery" return result correct', () => {
+    const { result } = renderHook(() => useGetProductDetailQuery('1'), {
+      wrapper: Wrapper,
+    });
+
+    expect(result.current).toMatchObject({
+      status: 'pending',
+      endpointName: 'getProductDetail',
+      isLoading: true,
+      isSuccess: false,
+      isError: false,
+      isFetching: true,
     });
   });
 });
